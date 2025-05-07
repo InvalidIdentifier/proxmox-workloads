5551b6f7f411bc582f4c739895edeee7fed42393
InvalidIdentifier <rz-nord@proton.me>
2025-05-07 08:06:38 +0200

    tempremoved k3s

--- terraform/node152.tf
+++ terraform/node152.tf
@@ -1,54 +1,54 @@
-resource "proxmox_vm_qemu" "vmQemuNode152" {
-    target_node = var.target_node_241
-    name        = "node152"
-    clone       = var.deb12_template_241
-    vmid        = "152"
+# resource "proxmox_vm_qemu" "vmQemuNode152" {
+#     target_node = var.target_node_241
+#     name        = "node152"
+#     clone       = var.deb12_template_241
+#     vmid        = "152"
 
-    cores       = 6
-    sockets     = 1
-    cpu         = "host"
-    memory      = 8192
-    boot        = "order=scsi0"
-    hastate     = "started"
-    hagroup     = "HA"
-    tags        = "k3s_server,infra"
-    scsihw      = "virtio-scsi-pci"
+#     cores       = 6
+#     sockets     = 1
+#     cpu         = "host"
+#     memory      = 8192
+#     boot        = "order=scsi0"
+#     hastate     = "started"
+#     hagroup     = "HA"
+#     tags        = "k3s_server,infra"
+#     scsihw      = "virtio-scsi-pci"
 
-    disks {
-        scsi {
-            scsi0 {
-                disk {
-                    cache      = "none"
-                    discard    = true
-                    emulatessd = true
-                    size       = 50
-                    storage    = "fast_data"
-                    replicate  = true
-                }
-            }
-        }
-        ide {
-            ide3 {
-                cloudinit {
-                  storage = "fast_data"
-                }
-            }
-        }
-    }
+#     disks {
+#         scsi {
+#             scsi0 {
+#                 disk {
+#                     cache      = "none"
+#                     discard    = true
+#                     emulatessd = true
+#                     size       = 50
+#                     storage    = "fast_data"
+#                     replicate  = true
+#                 }
+#             }
+#         }
+#         ide {
+#             ide3 {
+#                 cloudinit {
+#                   storage = "fast_data"
+#                 }
+#             }
+#         }
+#     }
 
-    network {
-        bridge = "vmbr0"
-        model  = "virtio"
-    }
+#     network {
+#         bridge = "vmbr0"
+#         model  = "virtio"
+#     }
 
-    os_type         = "cloud-init"
-    ipconfig0       = "ip=${var.vlan_infra}.152/24,gw=${var.vlan_infra}.1"
-    nameserver      = "${var.vlan_base}.1"
-    ciuser          = var.user_username
-    cipassword      = var.user_password
-    agent           = 1
-    sshkeys = <<EOF
-    ${var.ssh_server_pub_key}
-    ${var.ssh_laptop_pub_key}
-EOF
-}
+#     os_type         = "cloud-init"
+#     ipconfig0       = "ip=${var.vlan_infra}.152/24,gw=${var.vlan_infra}.1"
+#     nameserver      = "${var.vlan_base}.1"
+#     ciuser          = var.user_username
+#     cipassword      = var.user_password
+#     agent           = 1
+#     sshkeys = <<EOF
+#     ${var.ssh_server_pub_key}
+#     ${var.ssh_laptop_pub_key}
+# EOF
+# }
